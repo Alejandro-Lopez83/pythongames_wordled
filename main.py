a58f2a94a55fb016ef23153db092a8304592ca2f
Alejandro-Lopez83 <alopma83@gmail.com>
2024-10-14 16:55:53 +0100

    vamos

--- main.py
+++ main.py
@@ -14,27 +14,31 @@ try_count = 0
 
 while try_count < 7:
     
-    for i in range(len(play_word)):
+    """ for i in range(len(play_word)):
         if play_word[i] == user_word[i]:
             print('YES')
         else:
-            print('NO')
+            print('NO') """
     
-   
-    #for i in user_word:
-        #count_unique_letter_playword = play_word.count(i)
-        #count_unique_letter_userword = user_word.count(i)
+  
+    for i in user_word:
+        count_unique_letter_playword = play_word.count(i)
+        count_unique_letter_userword = user_word[0:letter_index+1].count(i)
+
 
         print(i)
         
         if i in play_word and i == play_word[letter_index]:
             print(f"the letter in position {letter_index + 1} is in the right place")
         elif i in play_word:
-            print(f"the letter in position{letter_index + 1} is in the incorrect place")
+            if count_unique_letter_userword>count_unique_letter_playword:
+                pass
+            else:
+                print(f"the letter in position{letter_index + 1} is in the incorrect place")
         else:
             print(f"the letter is not in the word")
         letter_index += 1
-
+    
 
 try_count += 1
 
