6616feac81437447d65f09307f1dcef4fc48d5d1
Alejandro-Lopez83 <alopma83@gmail.com>
2024-10-15 10:38:54 +0100

    1 version done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 word_list = ["bread", "fruit", "steak", "berry", "mango", "grape", "onion", "peach", "lemon", "beans"]
 play_word = random.choice( word_list)
+print(play_word)
 letter_index = 0
 count_unique_letter_playword = 0
 count_letter_iterations= 0
@@ -10,15 +11,28 @@ print("The word must contain 5 letters and it is related with food\n")
 
 try_count = 0
 
+def menu():
+    print(f"\n\n*********************************************************************")
+    print(f"**                            WELCOME TO                           ** ")
+    print(f"**********************          WORDLE         ********************** ")
+    print(f"**                                                                 ** ")
+    print(f"*********************************************************************\n\n")
+
+menu()
+
 while try_count < 6:
     letter_index=0
-    user_word = input("Type in your guess: ").lower().strip()
+    user_word = input("--> Type in your guess: ").lower().strip()
     while len(user_word) !=5:
-        print("The word MUST HAVE 5 letters\n")
-        user_word = input("Type in your guess: ").lower().strip()
+        print("\n            WARNING: The word MUST HAVE 5 letters :WARNING\n")
+        user_word = input("--> Type in your guess: ").lower().strip()
 
     if user_word==play_word:
-            print("YOU WIN!!")
+            print("\n                       ********************")
+            print("                       ********************")
+            print("                       **    YOU WIN!!   **")
+            print("                       ********************")
+            print("                       ********************\n")
             break
 
 
@@ -27,33 +41,39 @@ while try_count < 6:
         count_unique_letter_userword = user_word[0:letter_index+1].count(i)
  
 
-        print(i)
+        
         
         if i in play_word and i == play_word[letter_index]:
-            print(f"The letter in position {letter_index + 1} is in the right place")
+            print(f"The letter ", end="")
+            print("|",i,"|", end="")
+            print(f" in position {letter_index + 1} is in the right place")
         elif i in play_word:
             if count_unique_letter_userword>count_unique_letter_playword:
-                print(f"The letter {i} is already filled")
+                print(f"The letter ", end="")
+                print("|",i,"|", end="")
+                print(f" in position {letter_index + 1} is already covered")
             else:
-                print(f"The letter in position{letter_index + 1} is in the incorrect place")
+               print(f"The letter ", end="")
+               print("|",i,"|", end="")
+               print(f" in position {letter_index + 1} is in the incorrect place")
         else:
-            print(f"The letter is not in the word")
+            print(f"The letter ", end="")
+            print("|",i,"|", end="")
+            print(" is not in the word <-------------")
         
         letter_index += 1
 
     if user_word != play_word and try_count<5:
-        print(f"\nYOU HAVE {5-try_count} ATTEMPS LEFT")
-        print("TRY AGAIN WITH A NEW WORD")
+        print("\n\n        |                                   |")
+        print(f"        |      YOU HAVE {5-try_count} ATTEMPS LEFT      |")
+        print("        |     TRY AGAIN WITH A NEW WORD     |")
+        print("        |                                   |\n\n")
+
     else:
-        print("YOU LOSE")
+        print("\n\n                       ********************")
+        print("                       ********************")
+        print("                       **   YOU LOSE!!   **")
+        print("                       ********************")
+        print("                       ********************\n")
 
     try_count+=1
-        
-        
-    
-
-
-
-
-#if play_word in range(0,6) == user_word in range(0,6):
-    #print()
